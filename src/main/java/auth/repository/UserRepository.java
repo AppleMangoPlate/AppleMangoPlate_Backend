5e7fdc5fc43fa1aea0b2c60298c0dab80f48f0fc
jonghyeonjo99 <97875266+jonghyeonjo99@users.noreply.github.com>
2023-10-08 14:27:01 +0900

    [feat] #3 UserRepository 구현

--- src/main/java/auth/repository/UserRepository.java
+++ src/main/java/auth/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package auth.repository;
 
-public interface UserRepository {
+import auth.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByNickName(String nickName);
+    Optional<User> findByEmail(String email);
 }
