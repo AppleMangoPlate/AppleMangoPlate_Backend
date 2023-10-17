4eb559fa5f83f10cd6f4cdfe72059b783b004f32
이다빈 <dlekqls6204@naver.com>
2023-10-18 03:11:02 +0900

    [fix] #4 KakaoLoginResponse 수정

--- src/main/java/com/Applemango_Backend/auth/oauth/dto/KakaoLoginResponse.java
+++ src/main/java/com/Applemango_Backend/auth/oauth/dto/KakaoLoginResponse.java
@@ -1,10 +1,13 @@
 package com.Applemango_Backend.auth.oauth.dto;
 
 import com.Applemango_Backend.auth.domain.User;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class KakaoLoginResponse {
-    public boolean loginSuccess;
-    public User user;
+    private boolean loginSuccess;
+    private long userId;
+    private String email;
 }
