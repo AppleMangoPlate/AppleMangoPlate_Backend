eecf6e3d635739b23691edee2b1ad77f35f00fb6
jonghyeonjo99 <97875266+jonghyeonjo99@users.noreply.github.com>
2023-10-28 16:37:43 +0900

    [feat] #7 category search 구현

--- src/main/java/com/Applemango_Backend/search/service/CategoryService.java
+++ src/main/java/com/Applemango_Backend/search/service/CategoryService.java
@@ -20,41 +20,45 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 public class CategoryService {
-    private String server_url = "https://dapi.kakao.com/v2/local/search/keyword.JSON?category_group_code=FD6&size=10&query=";
+    private String server_url = "https://dapi.kakao.com/v2/local/search/keyword.JSON?category_group_code=FD6&size=15&query=";
 
     @Value("${search.key}")
     private String key;
 
     public List<JSONObject> categorization(String keyword,String classify) throws Exception{
         List<JSONObject> result = new ArrayList<>();
+
         //Header
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", key);
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         RestTemplate restTemplate = new RestTemplate();
-        URI url = URI.create(server_url + URLEncoder.encode(keyword,"UTF-8"));
-        RequestEntity<String> request = new RequestEntity<>(headers, HttpMethod.GET, url);
-        ResponseEntity<String> response = restTemplate.exchange(request, String.class);
 
         try {
-            JSONParser jsonParser = new JSONParser();
-            JSONObject jsonObject = (JSONObject) jsonParser.parse(response.getBody());
-            JSONArray documents = (JSONArray) jsonObject.get("documents");
+            for (int j = 1; j < 46; j++) {
+                URI url = URI.create(server_url + URLEncoder.encode(keyword, "UTF-8") + "&page=" + j);
+                RequestEntity<String> request = new RequestEntity<>(headers, HttpMethod.GET, url);
+                ResponseEntity<String> response = restTemplate.exchange(request, String.class);
 
-            for(int i = 0; i < 10; i++) {
-                JSONObject tmp = (JSONObject) documents.get(i);
-                String categorys = String.valueOf(tmp.get("category_name"));
-                String category = categorys.split(" > ")[1];
+                JSONParser jsonParser = new JSONParser();
+                JSONObject jsonObject = (JSONObject) jsonParser.parse(response.getBody());
+                JSONArray documents = (JSONArray) jsonObject.get("documents");
 
-                //카카오 검색결과 카테고리와 파라미터 카테고리가 같을 때
-                if(category.equals(classify)) {
-                    result.add(tmp);
+                for (int i = 0; i < 15; i++) {
+                    JSONObject tmp = (JSONObject) documents.get(i);
+                    String categorys = String.valueOf(tmp.get("category_name"));
+                    String category = categorys.split(" > ")[1];
+
+                    //카카오 검색결과 카테고리와 파라미터 카테고리가 같을 때
+                    if (category.equals(classify)) {
+                        result.add(tmp);
+                    }
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            } catch(Exception e){
+                e.printStackTrace();
+            }
 
         return result;
     }
