cd1e5fd75cd6d0414806915fe3dd084ab9a22db3
jonghyeonjo99 <97875266+jonghyeonjo99@users.noreply.github.com>
2023-11-11 18:40:08 +0900

    [feat] #23 BookmarkRestaurant 생성

--- src/main/java/com/Applemango_Backend/auth/dto/response/BookmarkDto.java
+++ src/main/java/com/Applemango_Backend/auth/dto/response/BookmarkDto.java
@@ -1,13 +1,21 @@
 package com.Applemango_Backend.auth.dto.response;
 
+import com.Applemango_Backend.auth.domain.Bookmark;
 import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class BookmarkDto {
     private Long bookmarkId;
     private String email;
     private List<BookmarkRestaurantDto> bookmarkRestaurants = new ArrayList<>();
+
+    public BookmarkDto(Bookmark bookmark) {
+        this.bookmarkId = bookmark.getId();
+        this.email = bookmark.getUser().getEmail();
+        this.bookmarkRestaurants = bookmark.getBookmarkRestaurants().stream().map(BookmarkRestaurantDto::new).collect(Collectors.toList());
+    }
 }
---
+++ src/main/java/com/Applemango_Backend/auth/dto/response/BookmarkRestaurantDto.java
@@ -0,0 +1,15 @@
+package com.Applemango_Backend.auth.dto.response;
+
+import com.Applemango_Backend.auth.domain.BookmarkRestaurant;
+
+public class BookmarkRestaurantDto {
+    private Long bookmarkRestaurantId;
+    private String restaurantName;
+    private String restaurantImage;
+
+    public BookmarkRestaurantDto(BookmarkRestaurant bookmarkRestaurant) {
+        this.bookmarkRestaurantId = bookmarkRestaurant.getId();
+        this.restaurantName = bookmarkRestaurant.getRestaurantName();
+        this.restaurantImage = bookmarkRestaurant.getRestaaurantImage();
+    }
+}
