26c97b9f3ac447635348aca069505146f41dd870
이다빈 <dlekqls6204@naver.com>
2023-11-19 02:37:14 +0900

    [feat] #30 review 엔티티 생성

--- src/main/java/com/Applemango_Backend/review/domain/Review.java
+++ src/main/java/com/Applemango_Backend/review/domain/Review.java
@@ -1,4 +1,38 @@
 package com.Applemango_Backend.review.domain;
 
+import com.Applemango_Backend.auth.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Review {
+
+    @Id @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    // 멤버와 관계 매핑
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // 가게 고유번호(카카오 api에서 제공하는 번호)
+    private String storeId;
+    private int rating; // 평점
+    private String content; // 리뷰내용
+    @CreatedDate
+    private LocalDateTime createDate; // 생성일
+    @LastModifiedDate
+    private LocalDateTime modifiedDate; // 수정일
+
 }
